a1ab9d5cc709a728bdc14e2a51a4cde5c73bfbf7
Jose Estrada <joseestradacobo@gmail.com>
2022-06-21 18:22:58 +0200

    .

--- python/main.py
+++ python/main.py
@@ -1,8 +1,13 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask,render_template,jsonify,request,send_from_directory
 from opengraph import OpenGraph
 app = Flask(__name__)
+@app.route('/', methods=['GET'])
 def root():
     return render_template('index.html')
+    
+@app.route('/assets/<path>')
+def send_report(path):
+    return send_from_directory('static/assets', path)
 
 @app.route('/link', methods=['GET'])
 def getParser():
